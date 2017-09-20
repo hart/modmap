feae53af2a0c35fd89134458b7031d1bfe1ac81a
Mark Zepeda <markzepedajr@gmail.com>
2017-09-20 12:18:26 -0700

    added tag support to 'set'
    added: unset, clear, byTag, and getTags

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
-const modules = {};
+const values = {};
+const tags = {};
 
-export const set = (key, module) => modules[key] = module;
-export const get = key => modules[key];
-export const all = () => Object.assign({}, modules);
+export const set = (key, value, ...tags) => {
+	values[key] = value;
+	if(tags) tags.forEach(tag => addToTag(tag, key));
+}
+
+export const get = key => values[key];
+
+export const all = () => Object.assign({}, values);
+
+export const unset = key => {
+	delete values[key];
+	Object.values(tags)
+	.forEach(tagMap => delete tagMap[key]);
+}
+
+export const clear = () => {
+	Object.keys(values).forEach(key => delete values[key]);
+	Object.keys(tags).forEach(tag => delete tags[tag]);
+}
+
+export const byTag = (tag) => {
+	const tagMap = tags[tag];
+
+	if(!tagMap) return {};
+
+	return Object.keys(tagMap)
+	.reduce((results, key) => { 
+		results[key] = values[key]; 
+		return results;
+	}, {});
+}
+
+export const getTags = () => Object.keys(tags);
 
 export default get;
+
+function addToTag(tag, key){
+	if(!tags[tag]) tags[tag] = {};
+	tags[tag][key] = true;
+}
--- tests/index-test.js
+++ tests/index-test.js
@@ -1,46 +1,158 @@
 import chai from 'chai'
 const should = chai.should();
 
-import { default as modmap, get, all, set} from 'src/';
+import { default as modmap, get, all, set, unset, clear, byTag, getTags } from 'src/';
 
 describe('modmap', function(){
-	const myModule = {
+	const NonExistentKey = 'InvalidFoos';
+
+	const FoosKey = 'Foos';
+	const FoosValue = {
 		awesome: true,
-		status: "spectacular"
+		status: 'spectacular'
 	}
 
-	describe('set', function(){
+	const FooBarKey = 'FooBar';
+	const FooBarValue = {
+		awesome: false,
+		status: 'not as spectacular'
+	}
+	const FooBarTag1 = 'FooBarTag123';
+	const FooBarTag2 = 77;
+	const FooBarTag3 = 'FooBars';
+	const NonExistentTag = "Cars"; 
+
+	describe('set()', function(){
 		it('should not throw an error', function(){
-			set("Foos", myModule);
+			should.not.throw(() => set(FoosKey, FoosValue));
+		});
+
+		it('should accept tags without an error', function(){
+			should.not.throw(() => set(FooBarKey, FooBarValue, FooBarTag1, FooBarTag2, FooBarTag3));
 		});
 	});
 
-	describe('get', function(){
+	describe('get()', function(){
 		it('should equal the default export', function(){
 			get.should.equal(modmap);
 		});
 
-		it('should return a value for a valid key', function(){
-			should.exist(get('Foos'));
+		it('should return a value for an existing key', function(){
+			should.exist(get(FoosKey));
 		});
 
-		it('should return null for an invalid key', function(){
-			should.not.exist(get('FoosTwo'));
+		it('should return null for a non-existent key', function(){
+			should.not.exist(get(NonExistentKey));
 		});
 
 		it('should return the correct value for a key', function(){
-			get('Foos').should.equal(myModule);
+			get(FoosKey).should.equal(FoosValue);
 		});
 	});
 
-	describe('all', function(){
+	describe('all()', function(){
 		it('should return an object of key/values', function(){
 			const allValues = all();
 			should.exist(allValues);
 			allValues.should.be.a('object');
-			
-			should.exist(allValues.Foos);
-			allValues.Foos.should.equal(myModule);
+		});
+
+		it('should return all key/values', function(){
+			const allValues = all();
+			should.exist(allValues[FoosKey]);
+			allValues[FoosKey].should.equal(FoosValue);
+			allValues[FooBarKey].should.equal(FooBarValue);
+		})
+	});
+
+	describe('getTags()', function(){
+		it('should not throw an error', function(){
+			should.not.throw(() => getTags());
+		});
+
+		it('should return an object', function(){
+			getTags().should.be.an('array');
+		});
+	})
+
+	describe('byTag()', function(){
+		set(FoosKey, FoosValue);
+		set(FooBarKey, FooBarValue, FooBarTag1, FooBarTag2, FooBarTag3)
+
+		it('should not throw an error with a non-existent tag', function(){
+			should.not.throw(() => byTag(NonExistentTag));
+		});
+
+		it('should not throw an error with an existing tag', function(){
+			should.not.throw(() => byTag(FooBarTag1));
+		});
+
+		it('should return an empty object for an non-existent tag', function(){
+			const values = byTag(NonExistentTag);
+			should.exist(values);
+			values.should.be.a('object');
+			values.should.be.empty;
+		});
+
+		it('should return a object of key/values for an existing tag', function(){
+			const values = byTag(FooBarTag1);
+			should.exist(values);
+			values.should.be.a('object');
+			values.should.not.be.empty;
+		});
+
+		it('should return correct key/values for an existing tag', function(){
+			const values = byTag(FooBarTag1);
+			const value = values[FooBarKey];
+			should.exist(value);
+			value.should.equal(FooBarValue);
+		});
+
+		it('should return a given key/value for each tag the key/value was set with', function(){
+			const results = 
+				[FooBarTag1, FooBarTag2, FooBarTag3]
+				.map(tag => byTag(tag))
+				.map(result => result[FooBarKey]);
+
+			results[0].should.equal(FooBarValue);
+			results[1].should.equal(FooBarValue);
+			results[2].should.equal(FooBarValue);
+		});
+	});
+
+	describe('unset()', function(){
+		it('should not throw an error with a non-existent key', function(){
+			should.not.throw(() => unset(NonExistentKey));
+		});
+
+		it('should not throw an error with an existing key', function(){
+			set(FoosKey, FoosValue);
+			should.not.throw(() => unset(FoosKey));
+		});
+
+		it('should remove the keyed value from the store', function(){
+			set(FoosKey, FoosValue);
+			should.exist(get(FoosKey));
+			unset(FoosKey);
+			should.not.exist(get(FoosKey));
+		});
+	});
+
+	describe('clear()', function(){
+		//make sure the map isn't empty
+		set(FoosKey, FoosValue);
+		set(FooBarKey, FooBarValue, FooBarTag1, FooBarTag2, FooBarTag3)
+
+		it('should not throw an error', function(){
+			should.not.throw(() => clear());
+		});
+
+		it('should remove all values from the map', function(){
+			all().should.be.empty;
+		});
+
+		it('should not throw an error while empty', function(){
+			should.not.throw(() => clear());
 		});
 	});
 });
